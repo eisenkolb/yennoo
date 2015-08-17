82a097a366741e4b7f57c4bac3cda7e680590861
Ronny Eisenkolb <eisenkolb.ronny@gmail.com>
2015-08-17 09:17:36 +0200

    Filter: Added dropdown menu for filterable items

--- controllers/index.navigation.js
+++ controllers/index.navigation.js
@@ -1,5 +1,40 @@
 Yennoo.controller("index.NavigationCtrl", ["$scope", "Navigation", function($scope, Navigation){
     $scope.entries = Navigation.GetEntries();
+    $scope.filters = {
+            title    : "Title",
+            year     : "Year",
+            genre    : "Genre",
+            dateadded: "Date Added",
+            runtime  : "Runtime",
+            rating   : "Rating",
+            studio   : "Studio"
+    };
+
+    /**
+     * Global filter object
+     *
+     * @type {{limit: number, reverse: boolean, property: string}}
+     */
+    $scope.$root.filter = {
+        limit   : 8,
+        reverse : false,
+        property: ""
+    };
+
+    /**
+     * Actions to set global filter properties
+     *
+     * @type {{limit: function, prop: function}}
+     */
+    $scope.filter = {
+        limit: function(limit){
+            $scope.$root.filter.limit = limit;
+        },
+        prop: function(prop){
+            $scope.$root.filter.property = prop;
+
+        }
+    };
 
     $scope.$on("loading:resources:complete", function(event, data){
         angular.element("#loading-mask .bar.progress").addClass("done");
