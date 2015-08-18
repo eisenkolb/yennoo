a550a927b5975c7412b8569f260d49a19a82f15c
Ronny Eisenkolb <eisenkolb.ronny@gmail.com>
2015-08-18 20:44:15 +0200

    Filter: Added functionality to reverse the pager result * Added js annotating for the navigation controller

--- controllers/index.navigation.js
+++ controllers/index.navigation.js
@@ -1,4 +1,5 @@
 Yennoo.controller("index.NavigationCtrl", ["$scope", "Navigation", function($scope, Navigation){
+    $scope.limits  = [24, 48, 96];
     $scope.entries = Navigation.GetEntries();
     $scope.filters = {
             title    : "Title",
@@ -16,7 +17,7 @@ Yennoo.controller("index.NavigationCtrl", ["$scope", "Navigation", function($sco
      * @type {{limit: number, reverse: boolean, property: string}}
      */
     $scope.$root.filter = {
-        limit   : Yennoo.setting.filter.limit    || 8,
+        limit   : Yennoo.setting.filter.limit    || $scope.limits[0],
         reverse : Yennoo.setting.filter.reverse  || false,
         property: Yennoo.setting.filter.property || ""
     };
@@ -24,23 +25,60 @@ Yennoo.controller("index.NavigationCtrl", ["$scope", "Navigation", function($sco
     /**
      * Actions to set global filter properties
      *
-     * @type {{limit: function, prop: function}}
+     * @type {{limit: function, property: function, reverse: function}}
      */
     $scope.filter = {
-        limit: function(limit){
+
+        /**
+         * Sets the limit of the pager result
+         *
+         * @param {?number} limit
+         */
+        limit: function(limit)
+        {
             $scope.$root.filter.limit = limit;
         },
-        prop: function(prop){
-            $scope.$root.filter.property = prop;
 
+        /**
+         * Sets the property for sorting the pager result
+         *
+         * @param {string} property
+         */
+        property: function(property)
+        {
+            $scope.$root.filter.property = property;
+        },
+
+        /**
+         * Reverse the order of the pager result
+         */
+        reverse: function()
+        {
+            /**
+             * @type {boolean}
+             */
+            $scope.$root.filter.reverse = !$scope.$root.filter.reverse;
         }
     };
 
-    $scope.$on("loading:resources:complete", function(event, data){
-        angular.element("#loading-mask .bar.progress").addClass("done");
+    /**
+     * Element of the loading mask
+     *
+     * @type {object|HTMLElement}
+     */
+    $scope.loadingMask = angular.element("#loading-mask .bar.progress");
+
+    /**
+     * Removes the displaying loading status
+     */
+    $scope.$on("loading:resources:complete", function(){
+        $scope.loadingMask.addClass("done");
     });
 
-    $scope.$on("loading:resources:start", function(event, data){
-        angular.element("#loading-mask .bar.progress").removeClass("done");
+    /**
+     * Adds the loading status on top of the page
+     */
+    $scope.$on("loading:resources:start", function(){
+        $scope.loadingMask.removeClass("done");
     });
 }]);
