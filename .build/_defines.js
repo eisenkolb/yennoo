1597a34ea755beacf87c2211056435473996c4ea
Ronny Eisenkolb <eisenkolb.ronny@gmail.com>
2015-10-27 20:42:50 +0100

    Travis CI: Updated script, environment variable and added api_key

--- .build/_defines.js
+++ .build/_defines.js
@@ -6,4 +6,4 @@ GLOBAL.jsxFiles = glob.sync("./**/*.js", {ignore: "./**/{node_modules,assets/lib
 GLOBAL.jsxCores = glob.sync("./assets/lib/*.js");
 
 GLOBAL.RELEASE = (process.env.TRAVIS_TAG || process.env.GIT_TAG || "0~devel").replace(/^v/, "");
-GLOBAL.PRODUCT = (process.env.product || "yennoo").toLowerCase();
+GLOBAL.PRODUCT = (process.env.PRODUCT || "yennoo").toLowerCase();
